ad05417d83138346e597932b81d3c299d4e1ea34
Ceren <cerenbulbul27@gmail.com>
2020-09-26 08:50:02 -0700

    Animation Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import "package:splashscreen/splashscreen.dart";
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Splash(),
+      home: AnimatedSplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -31,6 +32,79 @@ class SecondScreen extends StatelessWidget {
     );
   }
 }
+
+class AnimatedSplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => new SplashScreenState();
+}
+
+class SplashScreenState extends State<AnimatedSplashScreen>
+    with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  AnimationController animationController;
+  Animation<double> animation;
+
+  startTime() async {
+    var _duration = new Duration(seconds: 6);
+    return SecondScreen();
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+        vsync: this, duration: new Duration(seconds: 3));
+    animation =
+    new CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => this.setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(137, 168, 73, 1),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+
+
+            ],),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image.asset(
+                'images/logo_circle_big.png',
+                width: animation.value * 600,
+                height: animation.value * 600,
+              ),
+            ],
+          ),
+          new Align(alignment: Alignment.bottomCenter,
+            child: new Text("Camper Campus",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),)
+        ],
+      ),
+    );
+  }
+}
+
+
+
 
 
 
